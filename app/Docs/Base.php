0259cdceed22ad2227f3c3a5595b6e2d4ac9aedd
Haikal <mhaikalas@gmail.com>
2025-07-14 15:10:31 +0700

    update base swagger

--- app/Docs/Base.php
+++ app/Docs/Base.php
@@ -5,16 +5,17 @@ namespace App\Docs;
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Info(
- *  title="Todo API",
- *  version="1.0.0",
- *  description="Backend Lumen untuk technical‑test"
+ * @OA\OpenApi(
+ *   @OA\Info(
+ *     title="Todo API",
+ *     version="1.0.0",
+ *     description="Backend Lumen untuk technical‑test"
+ *   ),
+ *   @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Production"
+ *   )
  * )
- * @OA\Server(
- * url=L5_SWAGGER_CONST_HOST,
- *  description="Server"
- * )
- * @OA\PathItem(path="/") 
+ * @OA\PathItem(path="/")
  */
-
 class Base {}
