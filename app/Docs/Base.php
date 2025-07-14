b8404066ac8c343cad8fbdbbe4caf1f432dda954
Haikal <mhaikalas@gmail.com>
2025-07-14 12:41:31 +0700

    initial setup

---
+++ app/Docs/Base.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Docs;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *  title="Todo API",
+ *  version="1.0.0",
+ *  description="Backend Lumen untuk technical‑test"
+ * )
+ * @OA\Server(
+ * url=L5_SWAGGER_CONST_HOST,
+ *  description="Server"
+ * )
+ * @OA\PathItem(path="/") 
+ */
+
+class Base {}
